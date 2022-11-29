ed50c8b3873cd5ed110dd5cb2d39f7ca2995988c
Ezekiel <ehalle4@students.towson.edu>
2022-11-29 12:52:56 -0500

    Created footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <br />
+      <hr />
+      Copyright 2022 Pyxus
+      <br />
+      <hr />
+    </div>
+  );
+}
