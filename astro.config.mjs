d4f52d9e03c4d9c3b8437b3f9c06f41587a4202c
Ezekiel <ehalle4@students.towson.edu>
2022-11-29 00:13:53 -0500

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [react(), tailwind(), mdx(), sitemap()]
 });
